991cee71b7df35560058ce37e5bd1dbffd244f24
Eelco Dijkstra <eelco.j.dijkstra@gmail.com>
2016-02-02 10:10:02 +0100

    Added docenten to webdemo

--- webdemo.py
+++ webdemo.py
@@ -1,13 +1,28 @@
 import web
+import json
+import pymongo
+
+client = pymongo.MongoClient()
+db = client["test"]
 
 urls = (
-  '/', 'index'
+  '/', 'index',
+  '/docenten', 'docenten'
 )
 
 class index:
-    def GET(self):
-        return "Hello, world!"
+  def GET(self):
+    return "Hello, world!"
+
+class docenten:
+  def GET(self):
+    coll = db["docenten"]
+    result = []
+    for docent in coll.find():
+      result.push(docent)
+    return(json.dumps(result))
+
 
 if __name__ == "__main__":
-    app = web.application(urls, globals())
-    app.run()
+  app = web.application(urls, globals())
+  app.run()
