6877a7d01a36cade89da55b74b64dca30198ca0e
Eelco Dijkstra <eelco.j.dijkstra@gmail.com>
2016-02-02 09:54:06 +0100

    Extended demo.py

--- demo.py
+++ demo.py
@@ -6,11 +6,18 @@ client = pymongo.MongoClient()
 db = client.test # or: client["test"]
 
 # show all collections:
+print("Collections:)
 print(db.collection_names(include_system_collections=False))
 
 # use the 'docenten' collection:
 collection = db.docenten # or: db["docenten"]
 
 # show all documents in this collection:
+print("Alle docenten:")
 for docent in collection.find():
+  print(docent)
+
+# show all documents with the specified property:
+print("Informatica-docenten:")
+for docent in collection.find({"vak": "Informatica"}):
   print(docent)
