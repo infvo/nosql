181ee2e1b363b4327031c1b0362cae5279d3cb48
Eelco Dijkstra <eelco.j.dijkstra@gmail.com>
2016-02-03 21:30:03 +0100

    Extend webdemo1.py

--- webdemo1.py
+++ webdemo1.py
@@ -7,10 +7,18 @@ db = client["test"]
 
 urls = (
   '/', 'index',
-  '/docenten', 'docenten'
+  '/docenten', 'docenten',
+  '/form', 'form'
 )
 
 render = web.template.render('templates/')
+
+def addDocent(d):
+  db.docenten.replace_one(
+    {"naam": d["naam"]},
+    d,
+    True
+  )
 
 class index:
   def GET(self):
@@ -21,8 +29,22 @@ class docenten:
     coll = db["docenten"]
     result = []
     for docent in coll.find():
+      # normalize docent["vak"] to a list:
+      if not isinstance(docent["vak"], list):
+        docent["vak"] = [docent["vak"]]
       result.append(docent)
     return render.docenten(docenten=result)
+
+class form:
+  def GET(self):
+    return render.form()
+
+  def POST(self):
+    data = web.input()
+    # print(data)
+    data["vak"] = [x.strip() for x in data["vak"].split(",")]
+    addDocent(data)
+    return render.docenten(docenten=db.docenten.find())
 
 if __name__ == "__main__":
   app = web.application(urls, globals())
