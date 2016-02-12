dabaafcf75a203868e573bb49629a9f23d62d496
Eelco Dijkstra <eelco.j.dijkstra@gmail.com>
2016-02-12 09:59:20 +0100

    First (too simple) login pagen, with fixed user database

--- blog/blognotes.md
+++ blog/blognotes.md
@@ -5,3 +5,20 @@
 The use of sessions demonstrates the use of a persistent store. Individual requests do not have a common state: only the persistent store can be used for this.
 
 We can use MongoDB by providing a subclass of "Store", the class in the session module. The MongoDB store-subclass must provide an implentation for the interface as described in the Store class.
+
+### API
+
+* creation of a session: `mysession = web.session.Session(app, initializer)`
+* end a session: `mysession.kill()`
+
+(How to fill in the user name; how to get the user name?)
+
+A session may have data; this data can be stored and retrieved by:
+
+* `mysession.__getitem(key)`
+* `mysession.__setitem(key, value)`
+
+### HTTP authorization
+
+A disadvantage of this kind of authorization is that there is no well-defined "logout".
+
--- blog/session.py
+++ blog/session.py
@@ -1,21 +1,61 @@
 import web
+import pymongo
+import markdown
+
 web.config.debug = False
 urls = (
     "/count", "count",
+    "/login", "login",
     "/reset", "reset"
 )
 app = web.application(urls, locals())
 session = web.session.Session(app, web.session.DiskStore('sessions'), initializer={'count': 0})
 
+# DB configuration:
+client = pymongo.MongoClient()
+db = client["blog"]
+
+renderglobals = {'markdown': markdown.markdown}
+render = web.template.render('templates/', globals=renderglobals)
+
 class count:
-    def GET(self):
-        session.count += 1
-        return str(session.count)
+  def GET(self):
+    session.count += 1
+    return str(session.count)
 
 class reset:
-    def GET(self):
-        session.kill()
-        return ""
+  def GET(self):
+    session.kill()
+    return ""
+
+class login:
+  def GET(self):
+    return render.login()
+
+  def POST(self):
+    data=web.input()
+    print(data)
+    if data.user != "":
+      user = db.users.find_one({"username": data.user})
+      print(user)
+      if user != None:
+        if user["password"] == data.passwd:
+          return "Logged in: " + data.user
+    return "not logged in"
+
+# set default users"
+
+db.users.update_one(
+    {"username": "jon"},
+    {"$set": {"password": "pass1"}},
+    upsert=True
+  )
+
+db.users.update_one(
+    {"username": "jill"},
+    {"$set": {"password": "pass2"}},
+    upsert=True
+  )
 
 if __name__ == "__main__":
     app.run()
